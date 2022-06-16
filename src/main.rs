a4f1dca7fe4e8d58a100e0f85dc2a20bca227a72
Joseph Nawrocki <jonawrocki@gmail.com>
2022-06-15 17:43:54 -0700

    added squad slash command

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,142 @@
-use chrono::Utc;
 use dotenv::dotenv;
 use serenity::async_trait;
-use serenity::model::channel::Message;
-use serenity::model::prelude::ReactionType;
-use serenity::prelude::*;
+use serenity::Client;
+use serenity::model::prelude::Ready;
+use serenity::model::interactions::application_command::{
+    ApplicationCommand,
+    ApplicationCommandOptionType,
+    ApplicationCommandInteractionDataOptionValue
+};
+use serenity::model::interactions::{Interaction, InteractionResponseType};
+use serenity::prelude::{Context, EventHandler, GatewayIntents};
+use serenity::framework::standard::macros::group;
+use serenity::framework::standard::{StandardFramework};
 use std::env;
-pub mod squad;
-use crate::squad::{Member, Squad};
-use std::collections::HashMap;
-use std::str::FromStr;
-use serenity::Error;
+
+#[group]
+struct General;
 
 struct Handler;
 
-async fn create_posting(ctx: &Context, msg: &Message) -> Result<Message, Error> {
-    // Create new squad posting
-    msg.channel_id
-        .send_message(&ctx.http, |m| {
-            m.embed(|e| {
-                e.title("Assemble your squad!");
-                e.description(
-                    "✅ React to this message to ready up!\n\
-                    1️⃣ Use the number reacts to indicate for how many hours you are \
-                    available.\n\n\
-                    SquadBot will message you when at least <size> people are \
-                    ready.\n\n",
-                );
-                return e;
-            });
-            return m;
-        })
-        .await
-}
-
-fn create_options() -> HashMap<ReactionType, u8> {
-    HashMap::from([
-        (ReactionType::from_str("1️⃣").unwrap(), 1),
-        (ReactionType::from_str("2️⃣").unwrap(), 2),
-        (ReactionType::from_str("3️⃣").unwrap(), 3),
-        (ReactionType::from_str("4️⃣").unwrap(), 4),
-        (ReactionType::from_str("5️⃣").unwrap(), 5),
-        (ReactionType::from_str("6️⃣").unwrap(), 6),
-        (ReactionType::from_str("7️⃣").unwrap(), 7),
-        (ReactionType::from_str("8️⃣").unwrap(), 8),
-        (ReactionType::from_str("9️⃣").unwrap(), 9),
-    ])
-}
-
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!squad" {
-            // Check for errors in creating squad posting
-            let posting = match create_posting(&ctx, &msg).await {
-                Ok(p) => p,
-                Err(why) => {
-                    println!("Error creating posting: {:?}", why);
-                    return;
-                }
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+        let squad_command = ApplicationCommand::create_global_application_command(&ctx.http, |command| {
+            command
+                .name("squad")
+                .description("Create a new squad posting")
+                .create_option(|option| {
+                    option
+                        .name("size")
+                        .description("Number from 2 to 10")
+                        .kind(ApplicationCommandOptionType::Integer)
+                        .min_int_value(2)
+                        .max_int_value(10)
+                        .required(true)
+                })
+        })
+        .await;
+        println!("Pushed global slash command: {:#?}", squad_command);
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let content = match command.data.name.as_str() {
+                "squad" => {
+                    let options = command
+                        .data
+                        .options
+                        .get(0)
+                        .expect("Expected squad size.")
+                        .resolved
+                        .as_ref()
+                        .expect("Expected integer.");
+
+                    if let ApplicationCommandInteractionDataOptionValue::Integer(size) = options {
+                        size.to_string()
+                    } else {
+                        "Please provide a valid size.".to_string()
+                    }
+                },
+                _ => "Not implemented :(".to_string(),
             };
-            // Create first member of the squad
-            let member = Member {
-                user: &msg.author,
-                hours: 5,
-                expires: Utc::now(),
-            };
-            // Add member to members list
-            let mut members = vec![member];
-            // Create reaction options and corresponding values
-            let options = create_options();
-            // Create new squad
-            let squad = Squad {
-                posting: posting,
-                size: 5,
-                expires: Utc::now(),
-                channel: &msg.channel_id,
-                members: members,
-                options: options,
-            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|m| {
+                            m.embed(|e| {
+                                e.title("Assemble your squad!");
+                                e.description(
+                                    format!(
+                                        "✅ React to this message to ready up!\n\
+                                        1️⃣ Use the number reacts to indicate for how many hours you are \
+                                        available.\n\n\
+                                        SquadBot will message you when at least {} people are \
+                                        ready.\n\n", content)
+                                    );
+                                return e;
+                            });
+                            return m;
+                        })
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
         }
     }
 }
 
 #[tokio::main]
 async fn main() {
+    // Build framework
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Load Discord bot token
     dotenv().ok();
     let token = env::var("TOKEN").expect("token");
+
+    // Set Discord intents
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::GUILD_MESSAGE_REACTIONS
         | GatewayIntents::MESSAGE_CONTENT;
+
+    // Build client
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
+        .framework(framework)
         .await
         .expect("Client creation failed.");
+
+    // Start
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
 }
+
+// #[command]
+// async fn squad(ctx: &Context, msg: &Message) -> CommandResult {
+//     msg.channel_id
+//         .send_message(&ctx.http, |m| {
+//             m.embed(|e| {
+//                 e.title("Assemble your squad!");
+//                 e.description(
+//                     "✅ React to this message to ready up!\n\
+//                     1️⃣ Use the number reacts to indicate for how many hours you are \
+//                     available.\n\n\
+//                     SquadBot will message you when at least <size> people are \
+//                     ready.\n\n",
+//                 );
+//                 return e;
+//             });
+//             return m;
+//         })
+//         .await?;
+//     Ok(())
+// }
