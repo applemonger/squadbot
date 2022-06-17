f9a7780f4796ecb5f9ea9977631765e92f9ec604
Joseph Nawrocki <jonawrocki@gmail.com>
2022-06-17 11:49:49 -0700

    added ButtonChoice

--- src/main.rs
+++ src/main.rs
@@ -32,22 +32,36 @@ impl TypeMapKey for Redis {
     type Value = Arc<RwLock<redis::Client>>;
 }
 
-fn button(hours: u8) -> CreateButton {
+enum ButtonChoice {
+    Hours(u8),
+    Other(String)
+}
+
+fn button(choice: ButtonChoice) -> CreateButton {
     let mut b = CreateButton::default();
-    b.custom_id(hours.to_string().to_ascii_lowercase());
-    b.label(hours.to_string());
-    b.style(ButtonStyle::Primary);
+    match choice {
+        ButtonChoice::Hours(hours) => {
+            b.custom_id(hours.to_string().to_ascii_lowercase());
+            b.label(hours.to_string());
+            b.style(ButtonStyle::Primary);
+        },
+        ButtonChoice::Other(s) => {
+            b.custom_id(&s);
+            b.label(&s);
+            b.style(ButtonStyle::Danger);
+        }
+    }
     b
 }
 
 fn action_row() -> CreateActionRow {
     let mut ar = CreateActionRow::default();
     // We can add up to 5 buttons per action row
-    ar.add_button(button(1));
-    ar.add_button(button(2));
-    ar.add_button(button(3));
-    ar.add_button(button(4));
-    ar.add_button(button(5));
+    ar.add_button(button(ButtonChoice::Hours(1)));
+    ar.add_button(button(ButtonChoice::Hours(2)));
+    ar.add_button(button(ButtonChoice::Hours(3)));
+    ar.add_button(button(ButtonChoice::Hours(4)));
+    ar.add_button(button(ButtonChoice::Other("X".to_string())));
     ar
 }
 
