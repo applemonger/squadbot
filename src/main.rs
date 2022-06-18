93cb4b8c258de65974a4d2f712cdef380b601c3a
Joseph Nawrocki <jonawrocki@gmail.com>
2022-06-17 18:46:01 -0700

    created squad_command.rs

--- src/main.rs
+++ src/main.rs
@@ -2,91 +2,22 @@ use dotenv::dotenv;
 use serenity::async_trait;
 use serenity::framework::standard::macros::group;
 use serenity::framework::standard::StandardFramework;
-use serenity::model::interactions::application_command::{
-    ApplicationCommand, ApplicationCommandInteraction,
-    ApplicationCommandInteractionDataOptionValue, ApplicationCommandOptionType,
-};
-use serenity::model::interactions::{Interaction, InteractionResponseType};
 use serenity::model::prelude::message_component::MessageComponentInteraction;
-use serenity::model::prelude::{Message, Ready};
+use serenity::model::prelude::{Interaction, Ready};
 use serenity::prelude::{Context, EventHandler, GatewayIntents};
 use serenity::Client;
-use serenity::Error;
 use std::env;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 mod embed;
 mod redis_core;
+mod squad_command;
 
 #[group]
 struct General;
 
 struct Handler;
 
-async fn parse_squad_command(command: &ApplicationCommandInteraction) -> String {
-    let options = command
-        .data
-        .options
-        .get(0)
-        .expect("Expected squad size.")
-        .resolved
-        .as_ref()
-        .expect("Expected integer.");
-
-    if let ApplicationCommandInteractionDataOptionValue::Integer(size) = options {
-        size.to_string()
-    } else {
-        "Please provide a valid size.".to_string()
-    }
-}
-
-async fn respond_squad_command(
-    ctx: &Context,
-    command: &ApplicationCommandInteraction,
-    content: &String,
-) -> Result<Message, Error> {
-    command
-        .create_interaction_response(&ctx.http, |response| {
-            response
-                .kind(InteractionResponseType::ChannelMessageWithSource)
-                .interaction_response_data(|m| embed::build_embed(m, &content))
-        })
-        .await?;
-    command.get_interaction_response(&ctx.http).await
-}
-
-async fn register_squad_command(ctx: Context) -> Result<ApplicationCommand, Error> {
-    ApplicationCommand::create_global_application_command(&ctx.http, |command| {
-        command
-            .name("squad")
-            .description("Create a new squad posting")
-            .create_option(|option| {
-                option
-                    .name("size")
-                    .description("Number from 2 to 10")
-                    .kind(ApplicationCommandOptionType::Integer)
-                    .min_int_value(2)
-                    .max_int_value(10)
-                    .required(true)
-            })
-    })
-    .await
-}
-
-async fn handle_squad_command(ctx: &Context, command: &ApplicationCommandInteraction) {
-    let capacity = parse_squad_command(&command).await;
-    let command_result = respond_squad_command(&ctx, &command, &capacity).await;
-    match command_result {
-        Ok(response) => {
-            let mut con = redis_core::get_redis_connection(&ctx).await;
-            let message_id = response.id.as_u64().to_string();
-            redis_core::build_squad(&mut con, &message_id, &capacity).unwrap();
-        }
-        Err(_) => {
-            println!("Unable to respond to command.");
-        }
-    }
-}
 
 async fn handle_add_member(ctx: &Context, interaction: &MessageComponentInteraction, expires: u8) {
     let message_id = interaction.message.id.as_u64().to_string();
@@ -110,7 +41,7 @@ fn parse_message_component_interaction_id(
 impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
-        match register_squad_command(ctx).await {
+        match squad_command::register_squad_command(ctx).await {
             Ok(_) => println!("Registered global commands."),
             Err(error) => println!("Error registering global commands: {:?}", error),
         }
@@ -120,7 +51,7 @@ impl EventHandler for Handler {
         match interaction {
             Interaction::ApplicationCommand(command) => match command.data.name.as_str() {
                 "squad" => {
-                    handle_squad_command(&ctx, &command).await;
+                    squad_command::handle_squad_command(&ctx, &command).await;
                 }
                 _ => {
                     println!("Not implemented.");
---
+++ src/squad_command.rs
@@ -0,0 +1,76 @@
+use serenity::model::interactions::application_command::{
+    ApplicationCommand, ApplicationCommandInteraction,
+    ApplicationCommandInteractionDataOptionValue, ApplicationCommandOptionType,
+};
+use serenity::model::interactions::InteractionResponseType;
+use serenity::model::prelude::Message;
+use serenity::prelude::Context;
+use serenity::Error;
+use crate::embed;
+use crate::redis_core;
+
+
+async fn parse_squad_command(command: &ApplicationCommandInteraction) -> String {
+    let options = command
+        .data
+        .options
+        .get(0)
+        .expect("Expected squad size.")
+        .resolved
+        .as_ref()
+        .expect("Expected integer.");
+
+    if let ApplicationCommandInteractionDataOptionValue::Integer(size) = options {
+        size.to_string()
+    } else {
+        "Please provide a valid size.".to_string()
+    }
+}
+
+async fn respond_squad_command(
+    ctx: &Context,
+    command: &ApplicationCommandInteraction,
+    content: &String,
+) -> Result<Message, Error> {
+    command
+        .create_interaction_response(&ctx.http, |response| {
+            response
+                .kind(InteractionResponseType::ChannelMessageWithSource)
+                .interaction_response_data(|m| embed::build_embed(m, &content))
+        })
+        .await?;
+    command.get_interaction_response(&ctx.http).await
+}
+
+pub async fn register_squad_command(ctx: Context) -> Result<ApplicationCommand, Error> {
+    ApplicationCommand::create_global_application_command(&ctx.http, |command| {
+        command
+            .name("squad")
+            .description("Create a new squad posting")
+            .create_option(|option| {
+                option
+                    .name("size")
+                    .description("Number from 2 to 10")
+                    .kind(ApplicationCommandOptionType::Integer)
+                    .min_int_value(2)
+                    .max_int_value(10)
+                    .required(true)
+            })
+    })
+    .await
+}
+
+pub async fn handle_squad_command(ctx: &Context, command: &ApplicationCommandInteraction) {
+    let capacity = parse_squad_command(&command).await;
+    let command_result = respond_squad_command(&ctx, &command, &capacity).await;
+    match command_result {
+        Ok(response) => {
+            let mut con = redis_core::get_redis_connection(&ctx).await;
+            let message_id = response.id.as_u64().to_string();
+            redis_core::build_squad(&mut con, &message_id, &capacity).unwrap();
+        }
+        Err(_) => {
+            println!("Unable to respond to command.");
+        }
+    }
+}
