fbb2667b72fa67888795fc35cacf94a506fef470
Joseph Nawrocki <jonawrocki@gmail.com>
2022-06-19 18:10:13 -0700

    added comments to notify.rs

--- src/notify.rs
+++ src/notify.rs
@@ -3,11 +3,15 @@ use crate::redis_io;
 use serenity::client::Context;
 use serenity::model::prelude::Mention;
 
+/// DMs a notification to each member of every given squad (presumably filled squads).
 pub async fn notify_squads(ctx: &Context, con: &mut redis::Connection, squads: Vec<String>) {
     for squad in squads {
+        // Get members of squad and the channel of the squad posting
         let members = redis_io::get_members(con, &squad).unwrap();
         let channel = redis_io::get_channel(con, &squad).unwrap();
+        // Link to the channel in which the squad posting was made
         let channel_mention = format!("{}", Mention::from(channel));
+        // Include roster of squad members in each message
         let mut roster = String::new();
         for (key, value) in &members {
             let mention = format!("{}", Mention::from(*key));
@@ -15,6 +19,7 @@ pub async fn notify_squads(ctx: &Context, con: &mut redis::Connection, squads: V
             let line = &format!("{} available for {}\n", mention, ttl)[..];
             roster.push_str(line);
         }
+        // Send message to each squad member
         for (user_id, _ttl) in &members {
             let dm_channel = user_id.create_dm_channel(&ctx.http).await.unwrap();
             dm_channel
@@ -29,6 +34,7 @@ pub async fn notify_squads(ctx: &Context, con: &mut redis::Connection, squads: V
                 .await
                 .unwrap();
         }
+        // Mark the squad as filled
         redis_io::fill_squad(con, &squad).unwrap();
     }
 }
